562552190e0a229b5737ad6f36e669ad3f402bce
Allen Zhao <995029442@qq.com>
2018-11-29 18:44:32 +0800

    自定义 v-model

---
+++ custom-v-model/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Custom v-model="text" />
+  </div>
+</template>
+<script>
+  /**
+   * 加上 v-model 指令之后, 会默认绑定一个 change 事件 和向下传递一个 value 属性
+   * 
+   */
+
+  import Custom from './components/Custom'
+
+  export default {
+    components: {
+      Custom
+    },
+    data: () => ({
+      text: 'xxxx'
+    }),
+    created () {
+      setInterval(() => {
+        console.log(this.text)
+      }, 500)
+    }
+  }
+</script>
---
+++ custom-v-model/src/components/Custom.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="custom-input">
+    <input :value="value" @input="$emit('input', $event.target.value)" type="text">
+  </div>
+</template>
+<script>
+  export default {
+    props: ['value']
+  }
+</script>
