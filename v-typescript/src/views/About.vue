2a62402047ccc7fe727b9440aec3277a667346e3
Allen Zhao <995029442@qq.com>
2018-12-17 19:49:54 +0800

    modify

---
+++ v-typescript/src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>{{aboutTitle}}</h1>
+  </div>
+</template>
+<script lang="ts">
+  import { Emit, Inject, Model, Provide, Watch, Mixins, Component, Prop, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class About extends Vue {
+
+
+    @Prop({default: 'about v-typescript'}) aboutTitle!: string
+    @Prop(Number) count!: number
+    @Prop(Number) propA!: number
+
+    @Watch('name')
+    nameChange (val: string, oldVal: string) {
+      console.log('oldVal', oldVal)
+      console.log('val', val)      
+    }
+
+    created () {
+      console.log(this.propA)
+    }
+
+    @Emit('resetName')
+    reset () {
+      return 'xxx'
+    }
+
+
+  }
+
+</script>
