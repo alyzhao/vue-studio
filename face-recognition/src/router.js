890e77dd721e5fbc478bf4645527dc6fd3b3201c
Allen Zhao <995029442@qq.com>
2019-04-08 18:32:04 +0800

    modify

--- face-recognition/src/router.js
+++ face-recognition/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import SecondVerify from './views/SecondVerify'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/second',
+      name: 'second',
+      component: SecondVerify
     },
     {
       path: '/about',
---
+++ face-recognition/src/views/SecondVerify.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="second-verify">
+    <div class="verify-input">
+      <div>
+        <div class="pass-avartar"><img src="https://interactive-examples.mdn.mozilla.net/media/examples/plumeria.jpg"></div>
+        <PasswordInput :value="keyNumber" />
+      </div>
+    </div>
+    <div class="verify-keypad">
+      <Numerickeypad v-model="keyNumber" />
+    </div>
+  </div>
+</template>
+<script>
+  import Numerickeypad from '@/components/Numerickeypad'
+  import PasswordInput from '@/components/PasswordInput'
+
+  export default {
+    components: {
+      Numerickeypad,
+      PasswordInput,
+    },
+    data() {
+      return {
+        keyNumber: ''
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style lang="scss">
+  .second-verify {
+    background: center no-repeat url(../assets/images/bg_pattern.png);
+    background-size: cover;
+    height: 100%;
+    .verify-input {
+      height: 55%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .verify-keypad {
+      height: 45%;
+      display: flex;
+      justify-content: center;
+      .numeric-keypad {
+        height: 100%;
+        width: 400px;
+      }
+    }
+
+    .pass-avartar {
+      padding: 10px;
+      width: 155px;
+      height: 155px;
+      border-radius: 50%;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      box-sizing: border-box;
+      margin: 0 auto;
+      margin-bottom: 50px;
+      img {
+        // display: block;
+        object-fit: cover;
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+  }
+</style>
